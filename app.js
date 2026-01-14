49ce2c0e918e7117494d54641251e51e3a5a0478
xovar <xovar9663@gmail.com>
2026-01-14 23:34:37 +0600

    add error middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import {PORT} from './config/env.js';
 import connectToDataBase from "./database/mongodb.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import sbusriptionRouter from "./routes/subscription.routes.js";
+import errorMiddleware from "./middlewares/error.middlewares.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -11,9 +13,15 @@ app.get('/', (req,res)=> {
     res.send("Server Is Running")
 })
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser)
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscription', sbusriptionRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () =>{
     console.log('server is running on port 5500');
