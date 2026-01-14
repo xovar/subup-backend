eb58a71dabcb18fc550f3c79dcfe42d7f77c6b6c
xovar <xovar9663@gmail.com>
2026-01-15 01:52:55 +0600

    add sign in controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
@@ -47,3 +47,42 @@ export const signUp = async (req, res, next) => {
     session.endSession();
     next(error);
   }};
+
+export const signIn = async (req,res,next) => {
+    try {
+
+        const {email,password} = req.body;
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+
+        if(!isPasswordValid){
+            const error = new Error('Password is invalid');
+            error.statusCode = 401
+            throw(error)
+        }
+
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+            expiresIn: "7d",
+          });
+
+          res.status(200).json({
+            success: true,
+            message: "User Signed In Successfully",
+            data: {
+              token: token,
+              user: user,
+            },
+          }); 
+        
+    } catch (error) {
+        next(error)
+    }
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { signUp } from '../controllers/auth.controller.js';
+import { signIn, signUp } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
 
 authRouter.post('/sign-up', signUp);
 
-authRouter.post('/sign-in', (req,res) => res.send({message: "sign in"}));
+authRouter.post('/sign-in', signIn);
 
 authRouter.post('/sign-out', (req,res) => res.json({message: 'sign out'}));
 
